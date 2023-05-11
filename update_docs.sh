7e3e7eef1ce36fa11232d2f0ba68781bd3e78b73
vincentberenz <vincentberenz@users.noreply.github.com>
2023-05-11 14:52:24 +0000

    Deploying to gh-pages from @ AlexImmer/Laplace@f46de2dfa2fed4b7fa5c27dfa30de06bfaf7e817 🚀

--- update_docs.sh
+++ update_docs.sh
@@ -1,4 +1,6 @@
-rm -rf ./docs/laplace
-pdoc --html --output-dir ./docs --template-dir ./docs/template --force laplace
-mv ./docs/laplace/* ./docs/
-rm -rf ./docs/laplace/
+#rm -rf ./docs/laplace
+#pdoc --html --output-dir ./docs --template-dir ./docs/template --force laplace
+#mv ./docs/laplace/* ./docs/
+#rm -rf ./docs/laplace/
+imaginary command
+ls -alh
